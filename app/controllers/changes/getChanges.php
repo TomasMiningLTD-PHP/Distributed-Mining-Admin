33763deecb4cbb7c7ee7073be7a8f9f2ab735166
Kim <kimpersson88@gmail.com>
2014-03-23 20:19:47 +0100

    fixed getChanges

--- app/controllers/changes/getChanges.php
+++ app/controllers/changes/getChanges.php
@@ -1,8 +1,15 @@
 <?php
 require_once(dirname(__FILE__).'/../../models/includes.php');
 function _getChanges() {
-        $tmp = array("120.0.0.1","43.546.23.12","34.6.23.677","231.435.23.1");
-	echo json_encode($tmp);
+	
+	Database::initDb();
+	$pools = Pool::findAll();
+	$urls = array();
+	foreach($pools as $pool) {
+		$urls[] = $pool->url;
+
+	}
+	echo json_encode($urls);
 }
 
 
